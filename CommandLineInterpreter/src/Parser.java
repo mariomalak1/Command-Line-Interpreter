e586512e24ab07473014ee45b8f49aecfe0a7b04
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-10-25 18:46:18 +0300

    first structure for the project

---
+++ CommandLineInterpreter/src/Parser.java
@@ -0,0 +1,13 @@
+public class Parser {
+    String commandName;
+    String[] args;
+
+    //This method will divide the input into commandName and args
+    //where "input" is the string command entered by the user
+    public boolean parse(String input){return false;}
+
+    public String getCommandName(){return "";}
+
+    public String[] getArgs(){return new String[0];}
+
+}
