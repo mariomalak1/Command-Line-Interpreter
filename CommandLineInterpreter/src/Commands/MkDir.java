430c9d7a22b3a5a41acb960e5f1e2ba14ea433f4
Ehabp1 <ehabashraf694@gmail.com>
2023-11-03 19:14:43 +0200

    fix error in mkdir

--- CommandLineInterpreter/src/Commands/MkDir.java
+++ CommandLineInterpreter/src/Commands/MkDir.java
@@ -3,36 +3,55 @@ package Commands;
 import java.io.File;
 
 public class MkDir implements ICommand{
-    String str;
+    String [] str ;
     @Override
     public Boolean isValidArgs(String[] args) {
-        return args.length == 1;
+        return args.length >= 1;
     }
 
     @Override
     public void runCommand() {
+        File [] directory = new File[str.length];
+        if(str.length>1){
 
-        File directory = new File(System.getProperty("user.dir"));
-
-        try {
-            if (directory.mkdir()) {
-                System.out.println("Directory is created");
-            } else {
-                System.out.println("Directory cannot be created");
+            for(int i =0;i<str.length;i++){
+                 directory[i] = new File(str[i]).getAbsoluteFile();
             }
-        } catch (SecurityException e) {
-            System.out.println("Encountered a security exception: " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Encountered an exception: " + e.getMessage());
         }
+        else{
+             directory[0] = new File(str[0]).getAbsoluteFile();
+        }
+        for(int i=0; i<str.length;i++){
+            try {
+                if (directory[i].mkdir()) {
+                    System.out.println("Directory is created");
+                } else {
+                    System.out.println("Directory cannot be created");
+                }
+            } catch (SecurityException e) {
+                System.out.println("Encountered a security exception: " + e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Encountered an exception: " + e.getMessage());
+            }
+
+        }
+
     }
 
 
 
     @Override
     public void PutArgs(String[] args) throws Exception {
+        str =new String[args.length];
         if (isValidArgs(args)){
-            str = args[0];
+            if(args.length >= 1){
+                for(int i =0;i<args.length;i++){
+                    str[i] = args[i];
+                }
+
+            }
+
+
         }
         else{
             throw new CommandsException("InValid Args");
